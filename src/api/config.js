9facb17c51bbb4712923335f7b6cf9ef08d63a0f
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-08-14 15:56:13 +0300

    send token in  request interceptor of axios instance

--- src/api/config.js
+++ src/api/config.js
@@ -4,4 +4,14 @@ const BASE_URL = process.env.REACT_APP_API_URL
 
 const axios_instance = axios.create({baseURL: BASE_URL})
 
+axios_instance.interceptors.request.use(
+    (config) => {
+      const token = localStorage.getItem('token');
+      if (token) {
+        config.headers.Authorization = `Bearer ${token}`;
+      }
+      return config;
+    }
+  );
+
 export default axios_instance ;

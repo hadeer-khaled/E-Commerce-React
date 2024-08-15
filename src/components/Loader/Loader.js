561f61c6d0f9110ce376002f1ecd704b2a47a124
hadeer-khaled <hadeer.awad02@eng-st.cu.edu.eg>
2024-08-15 14:01:05 +0300

    Show Product

---
+++ src/components/Loader/Loader.js
@@ -0,0 +1,7 @@
+export default function Loader() {
+  return (
+    <div className="flex justify-center items-center h-screen">
+      <div className="spinner-border animate-spin inline-block w-8 h-8 border-4 rounded-full border-primary border-t-transparent"></div>
+    </div>
+  );
+}
--- src/features/Product/pages/Show.js
+++ src/features/Product/pages/Show.js
@@ -1,7 +1,71 @@
 import { useParams } from "react-router-dom";
+import { getProductById } from "api/product";
+import { useEffect, useState } from "react";
+import Loader from "components/Loader/Loader";
 
 export default function Show() {
   const { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const [loading, setLoading] = useState(true);
 
-  return <div>Show {id}</div>;
+  useEffect(() => {
+    const fetchProduct = async () => {
+      try {
+        const response = await getProductById(id);
+        console.log(response.data.data);
+        setProduct(response.data.data);
+      } catch (error) {
+        console.error("Error fetching product:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchProduct();
+  }, [id]);
+
+  if (loading) {
+    return <Loader />;
+  }
+
+  if (!product) {
+    return <div>Error: Product not found</div>;
+  }
+  const images = product.images && product.images.length > 0 ? product.images : ['https://gebelesebeti.ge/front/asset/img/default-product.png'];
+
+  return (
+    <div className="container m-auto">
+      <div className="card card-compact bg-base-100 w-96 shadow-xl">
+        <div className="card-body">
+          <h2 className="card-title">{product.title}</h2>
+          <div className="badge badge-primary badge-outline">{product.price} EGP</div>
+
+          <div className="carousel w-full">
+            {images?.map((image, index) => (
+              <div
+                key={index}
+                id={`item${index + 1}`}
+                className="carousel-item w-full"
+              >
+                <img
+                  src={image}
+                  alt={`Product Image ${index + 1}`}
+                  className="w-full"
+                />
+              </div>
+            ))}
+          </div>
+
+          <div className="flex w-full justify-center gap-2 py-2">
+            {product.images?.map((_, index) => (
+              <a key={index} href={`#item${index + 1}`} className="btn btn-xs">
+                {index + 1}
+              </a>
+            ))}
+          </div>
+          <div>{product.description}</div>
+        </div>
+      </div>
+    </div>
+  );
 }
